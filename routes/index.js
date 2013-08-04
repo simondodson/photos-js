5aa4d5e8c53075bd48ad4c35f858df5482bf78df
Jesse Patching <jesse@patching.ca>
2013-08-04 14:53:34 -0600

    Sort the albums by date, descending

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 exports.index = function(req, res){
-    var Gallery = require("../models/gallery");
+    var Gallery = require("../models/gallery"),
+        _ = require('underscore');
 
     Gallery.find().exec(function (err, albums) {
+        // Sort the albums by date
+        albums = _.sortBy(albums, function (album) {
+            return -1 * album.date;
+        });
+
         res.render('index', {
             albums: albums,
             placeholderImg: process.env.PLACEHOLDER_IMAGE_URL,

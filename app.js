53b19f0df1661bc371bfb3cfe68652ad85d2007e
Jesse Patching <jesse@patching.ca>
2013-08-04 14:25:51 -0600

    Prevent protected pages from being cached by the browser

--- app.js
+++ app.js
@@ -62,6 +62,9 @@ app.locals = {
 
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) {
+        // Prevent protected pages from being cached
+        res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
+
         return next();
     }
 

71395618d2491144266ab7fbd377074f1dc595e4
Jesse Patching <jesse@patching.ca>
2013-08-05 11:15:38 -0600

    Added crude confirmation to delete album

--- public/js/script.js
+++ public/js/script.js
@@ -8,4 +8,14 @@ $(document).ready(function () {
         // Start that lazy loadin'
         $('.img-thumbnail:visible').unveil(200);
     }
+
+    $('.button-delete-album').click(function (e) {
+        var albumName = $(this).attr('data-album-name');
+        if (confirm("Are you sure you want to delete " + albumName)) {
+            // Allow the link click to propagate
+            return true;
+        } else {
+            e.preventDefault();
+        };
+    })
 });

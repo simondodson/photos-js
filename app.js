7d6d920e93b1235fad7ab348c9540b6d1f46dd67
Jesse Patching <jesse@patching.ca>
2013-08-14 07:48:01 -0600

    Added error handling for 404/500 from http://www.itamarweiss.com/post/57962670227/error-handling-in-node-js-express

--- app.js
+++ app.js
@@ -68,6 +68,36 @@ app.use(function (req, res, next) {
 });
 app.use(app.router);
 
+app.use(function(req, res, next){
+    res.status(404);
+
+    // respond with html page
+    if (req.accepts('html')) {
+        res.render('error', {
+            title: "Page Not Found",
+            message: req.url
+        });
+        return;
+    }
+
+    // respond with json
+    if (req.accepts('json')) {
+        res.send({ error: 'Not found' });
+        return;
+    }
+
+    // default to plain-text. send()
+    res.type('txt').send('Not found');
+});
+
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    res.render('error', {
+        title: "Internal Server Error",
+        message: err
+    });
+});
+
 process.on('uncaughtException', function(err) {
     console.log(err.stack);
 });
